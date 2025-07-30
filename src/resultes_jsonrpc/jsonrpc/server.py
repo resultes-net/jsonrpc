69b34e729820689feff03996ca85d7f2d2c8f8d9
Damian Birchler <damian@damianbirchler.net>
2025-07-30 15:48:42 +0200

    Almost there.

--- src/resultes_jsonrpc/jsonrpc/server.py
+++ src/resultes_jsonrpc/jsonrpc/server.py
@@ -1,3 +1,6 @@
+import abc as _abc
+import asyncio as _asyncio
+import contextlib as _ctx
 import logging as _log
 import typing as _tp
 
@@ -8,18 +11,91 @@ import resultes_jsonrpc.jsonrpc.types as _tps
 _LOGGER = _log.getLogger(__file__)
 
 
+class DispatcherBase(_abc.ABC):
+    @_abc.abstractmethod
+    async def dispatch(
+        self, data: str, context: _tp.Any, websocket: _tps.WebSocket
+    ) -> None:
+        raise NotImplementedError()
+
+
+class Dispatcher(DispatcherBase):
+    async def dispatch(
+        self, data: str, context: _tp.Any, websocket: _tps.WebSocket
+    ) -> None:
+        if result := await _jrpcs.async_dispatch(data, context):
+            await websocket.send_str(result)
+
+
+class _TerminateTaskGroupException(Exception):
+    pass
+
+
+class TaskSpawningDispatcher(
+    DispatcherBase, _ctx.AbstractAsyncContextManager["TaskSpawningDispatcher"]
+):
+    def __init__(self) -> None:
+        self._dispatcher = Dispatcher()
+        self._task_group: _asyncio.TaskGroup | None = None
+
+    async def __aenter__(self) -> _tp.Self:
+        if self._task_group:
+            raise RuntimeError("Already started.")
+
+        self._task_group = _asyncio.TaskGroup()
+
+        await self._task_group.__aenter__()
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> bool:
+        if not self._task_group:
+            raise RuntimeError("Not started.")
+
+        try:
+            await self._task_group.__aexit__(exc_type, exc_value, traceback)
+        except* _TerminateTaskGroupException:
+            pass
+
+        return False
+
+    async def dispatch(
+        self, data: str, context: _tp.Any, websocket: _tps.WebSocket
+    ) -> None:
+        if not self._task_group:
+            raise RuntimeError("Not entered.")
+
+        coroutine = self._dispatcher.dispatch(data, context, websocket)
+
+        task = self._task_group.create_task(coroutine)
+
+        _LOGGER.info("New task %s for dispatching request created.", task.get_name())
+
+    async def cancel_tasks(self) -> None:
+        _LOGGER.info("Terminating task group.")
+
+        if not self._task_group:
+            raise RuntimeError("Not entered.")
+
+        async def raise_exception() -> _tp.NoReturn:
+            raise _TerminateTaskGroupException()
+
+        self._task_group.create_task(raise_exception())
+
+
 class JsonRpcServer(_rjjb.JsonRpcBase):
     def __init__(
         self,
         websocket: _tps.WebSocket,
+        dispatcher: DispatcherBase = Dispatcher(),
         wakeup_period_seconds: float = 5.0,
         message_dispatch_context: _tp.Any = None,
     ) -> None:
         super().__init__(websocket, wakeup_period_seconds)
         self._message_dispatch_context = message_dispatch_context
+        self._dispatcher = dispatcher
 
     async def _handle_message(self, data: str) -> None:
-        if result := await _jrpcs.async_dispatch(
-            data, context=self._message_dispatch_context
-        ):
-            await self._websocket.send_str(result)
+        await self._dispatcher.dispatch(
+            data, self._message_dispatch_context, self._websocket
+        )
---
+++ src/resultes_jsonrpc/websockets/server.py
@@ -0,0 +1,59 @@
+import asyncio as _asyncio
+import collections.abc as _cabc
+import contextlib as _ctx
+import dataclasses as _dc
+import typing as _tp
+
+import aiohttp.web as _ahttpw
+
+
+@_dc.dataclass
+class WebsocketWithPath:
+    websocket: _ahttpw.WebSocketResponse
+    path: str
+
+
+class Server:
+    def __init__(self) -> None:
+        self._queue = _asyncio.Queue[WebsocketWithPath]()
+
+    @_ctx.asynccontextmanager
+    @staticmethod
+    async def start(
+        port: int, paths: _cabc.Sequence[str]
+    ) -> _cabc.AsyncIterator["Server"]:
+        server = Server()
+
+        app = _ahttpw.Application()
+
+        for path in paths:
+            app.add_routes([_ahttpw.get(path, server._create_websocket)])
+
+        runner = _ahttpw.AppRunner(app)
+        await runner.setup()
+        site = _ahttpw.TCPSite(runner, port=port)
+        await site.start()
+
+        yield server
+
+        await site.stop()
+        await runner.shutdown()
+
+    async def _create_websocket(
+        self, request: _ahttpw.Request
+    ) -> _ahttpw.WebSocketResponse:
+        websocket = _ahttpw.WebSocketResponse()
+        await websocket.prepare(request)
+        item = WebsocketWithPath(websocket, request.path)
+        await self._queue.put(item)
+        return websocket
+
+    async def websockets(self) -> _cabc.AsyncIterable[WebsocketWithPath]:
+        try:
+            while True:
+                yield await self._queue.get()
+        except _asyncio.QueueShutDown:
+            pass
+
+    def stop(self) -> None:
+        self._queue.shutdown(immediate=True)
