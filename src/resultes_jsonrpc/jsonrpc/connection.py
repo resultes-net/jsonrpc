efacb269e947a41e0e04b8a6c7db13112277637f
Damian Birchler <damian.birchler@ost.ch>
2025-12-11 16:07:28 +0100

    Send argument name together with value.

--- src/resultes_jsonrpc/jsonrpc/connection.py
+++ src/resultes_jsonrpc/jsonrpc/connection.py
@@ -2,6 +2,7 @@ import abc as _abc
 import asyncio as _asyncio
 import collections.abc as _cabc
 import contextlib as _ctx
+import dataclasses as _dc
 import functools as _ft
 import json as _json
 import logging as _log
@@ -115,6 +116,12 @@ class AsyncTaskSpawningDispatcher(DispatcherBase):
             await websocket.send_str(result)
 
 
+@_dc.dataclass
+class Argument:
+    name: str
+    value: _pyd.BaseModel
+
+
 class Connection(_rjwt.MessageReceiver):
     def __init__(
         self,
@@ -207,9 +214,13 @@ class Connection(_rjwt.MessageReceiver):
         await self._websocket.send_str(data)
 
     async def send_notification_base_model(
-        self, method: str, params: _pyd.BaseModel | None = None
+        self, method: str, argument: Argument | None = None
     ) -> None:
-        data = params.model_dump() if params else None
+        if argument:
+            value_data = argument.value.model_dump()
+            data = {argument.name: value_data}
+        else:
+            data = None
 
         await self.send_notification_data(method, data)
 
