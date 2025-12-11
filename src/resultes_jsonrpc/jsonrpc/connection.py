812ba1c5987693d9dff9f05bd7899d58c6e9847a
Damian Birchler <damian.birchler@ost.ch>
2025-12-11 15:42:27 +0100

    Merge client and server into connection.

--- src/resultes_jsonrpc/jsonrpc/connection.py
+++ src/resultes_jsonrpc/jsonrpc/connection.py
@@ -1,40 +1,22 @@
 import abc as _abc
 import asyncio as _asyncio
 import collections.abc as _cabc
+import contextlib as _ctx
 import functools as _ft
+import json as _json
 import logging as _log
 import traceback as _tb
 import typing as _tp
 
-import pydantic as _pyd
+import jsonrpcclient as _jrpcc
 import jsonrpcserver as _jrpcs
 import jsonrpcserver.codes as _jrpcsc
-import resultes_jsonrpc.websockets.types as _rjwt
+import pydantic as _pyd
 import resultes_jsonrpc.jsonrpc.types as _rjjt
+import resultes_jsonrpc.jsonrpc.types as _tps
+import resultes_jsonrpc.websockets.types as _rjwt
 
 _LOGGER = _log.getLogger(__file__)
-
-
-class ContextBase:
-    def __init__(self) -> None:
-        self._tasks = set[_asyncio.Task[_tp.Any]]()
-
-    def tasks(self) -> _cabc.Sequence[_asyncio.Task[_tp.Any]]:
-        return list(self._tasks)
-
-    def register_task(self, task: _asyncio.Task[_tp.Any]) -> None:
-        if task in self._tasks:
-            raise ValueError("Already registered.")
-
-        self._tasks.add(task)
-
-        task.add_done_callback(self._unregister_task)
-
-    def _unregister_task(self, task: _asyncio.Task[_tp.Any]) -> None:
-        if task not in self._tasks:
-            raise ValueError("Not registered.")
-
-        self._tasks.remove(task)
 
 
 type AsyncJsonRpcMethod[**P] = _cabc.Callable[P, _cabc.Awaitable[_jrpcs.Result]]
@@ -133,16 +115,111 @@ class AsyncTaskSpawningDispatcher(DispatcherBase):
             await websocket.send_str(result)
 
 
-class JsonRpcServer(_rjwt.MessageReceiver):
+class Connection(_rjwt.MessageReceiver):
     def __init__(
         self,
-        websocket: _rjwt.WriteWebsocket,
         dispatcher: DispatcherBase,
         context: _tp.Any,
+        websocket: _rjwt.WriteWebsocket,
     ) -> None:
-        self._websocket = websocket
         self._context = context
         self._dispatcher = dispatcher
+        self._websocket = websocket
+
+        self._new_message_received_event = _asyncio.Event()
+        self._parsed_response_futures_by_request_id = dict[
+            int, _asyncio.Future[_jrpcc.responses.Response]
+        ]()
 
     async def on_message_received(self, data: str) -> None:
+        is_request = "error" in data or "result" in data
+        if is_request:
+            await self._on_request_received(data)
+        else:
+            await self._on_response_received(data)
+
+    async def _on_request_received(self, data: str) -> None:
         await self._dispatcher.dispatch(data, self._context, self._websocket)
+
+    async def _on_response_received(self, data: str) -> None:
+        parsed_response = _jrpcc.parse_json(data)
+
+        parsed_responses = (
+            [parsed_response]
+            if isinstance(parsed_response, (_jrpcc.Ok, _jrpcc.Error))
+            else list(parsed_response)
+        )
+
+        for parsed_response in parsed_responses:
+            future = self._parsed_response_futures_by_request_id[parsed_response.id]
+            future.set_result(parsed_response)
+
+    async def send_request_and_check_and_get_response(
+        self, method: str, params: _tps.JsonStructured | None = None
+    ) -> _tps.Json:
+        converted_params = self._convert_params(params)
+
+        request: _tps.Request = _jrpcc.request(method=method, params=converted_params)
+
+        data = _json.dumps(request)
+
+        _LOGGER.debug("Sending request %s.", data)
+        await self._websocket.send_str(data)
+
+        response = await self._get_response(request["id"])
+        _LOGGER.debug("Got response %s.", response)
+
+        match response:
+            case _jrpcc.Ok(result):
+                return result
+            case _jrpcc.Error() as error:
+                _LOGGER.error("Got error %s: %s", error.message, error.data)
+                raise RuntimeError(error)
+            case _:
+                _tp.assert_never(_)
+
+    async def _get_response(self, request_id: int) -> _jrpcc.responses.Response:
+        async with self._registered_future(request_id) as future:
+            response = await future
+            return response
+
+    @_ctx.asynccontextmanager
+    async def _registered_future(
+        self, request_id: int
+    ) -> _cabc.AsyncIterator[_asyncio.Future[_jrpcc.responses.Response]]:
+        future = _asyncio.Future[_jrpcc.responses.Response]()
+        self._parsed_response_futures_by_request_id[request_id] = future
+
+        yield future
+
+        del self._parsed_response_futures_by_request_id[request_id]
+
+    async def send_notification_data(
+        self, method: str, params: _tps.JsonStructured | None = None
+    ) -> None:
+        converted_params = self._convert_params(params)
+
+        notification = _jrpcc.notification(method=method, params=converted_params)
+
+        data = _json.dumps(notification)
+
+        _LOGGER.debug("Sending notification %s.", data)
+        await self._websocket.send_str(data)
+
+    async def send_notification_base_model(
+        self, method: str, params: _pyd.BaseModel | None = None
+    ) -> None:
+        data = params.model_dump() if params else None
+
+        await self.send_notification_data(method, data)
+
+    def _convert_params(
+        self, params: _tps.JsonStructured | None
+    ) -> tuple[_tps.Json, ...] | _cabc.Mapping[str, _tps.Json] | None:
+        if not params:
+            return None
+
+        try:
+            return dict(params)
+        except ValueError:
+            return tuple(params)
