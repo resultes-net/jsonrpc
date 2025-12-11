c5758c4ceb1f1b962befb3a463090104fdff0585
Damian Birchler <damian.birchler@ost.ch>
2025-12-11 10:09:52 +0100

    Add validating `jsonrpcserver` method decorator.

--- src/resultes_jsonrpc/jsonrpc/server.py
+++ src/resultes_jsonrpc/jsonrpc/server.py
@@ -6,9 +6,11 @@ import logging as _log
 import traceback as _tb
 import typing as _tp
 
+import pydantic as _pyd
 import jsonrpcserver as _jrpcs
 import jsonrpcserver.codes as _jrpcsc
-import resultes_jsonrpc.websockets.types as _tps
+import resultes_jsonrpc.websockets.types as _rjwt
+import resultes_jsonrpc.jsonrpc.types as _rjjt
 
 _LOGGER = _log.getLogger(__file__)
 
@@ -59,17 +61,56 @@ def cancellable_async_jrpcs_method[**P](
     return nested
 
 
+def cancellable_async_validated_jrpcs_method[T: _pyd.BaseModel, C](
+    clazz: type[T],
+) -> _cabc.Callable[
+    [AsyncJsonRpcMethod[[C, T]]], AsyncJsonRpcMethod[[C, _rjjt.JsonObject]]
+]:
+    def create_validating_method(
+        validated_method: AsyncJsonRpcMethod[[C, T]],
+    ) -> AsyncJsonRpcMethod[[C, _rjjt.JsonObject]]:
+        @_jrpcs.method()
+        @_ft.wraps(validated_method)
+        async def validating_method(
+            data: _rjjt.JsonObject, context: C
+        ) -> _jrpcs.Result:
+            try:
+                instance = clazz(**data)
+            except _pyd.ValidationError as validation_error:
+                errors = validation_error.errors()
+                return _jrpcs.InvalidParams(errors)
+
+            try:
+                return await validated_method(context, instance)
+            except _asyncio.CancelledError:
+                _LOGGER.warning("The request was cancelled on the server.")
+                return _jrpcs.Error(
+                    _jrpcsc.ERROR_SERVER_ERROR,
+                    "The request was cancelled on the server.",
+                )
+            except Exception as exception:
+                _LOGGER.error("Exception occurred: %s", exc_info=exception)
+                traceback = "\n".join(_tb.format_exception(exception))
+                return _jrpcs.Error(
+                    _jrpcsc.ERROR_SERVER_ERROR, str(exception), traceback
+                )
+
+        return validating_method
+
+    return create_validating_method
+
+
 class DispatcherBase(_abc.ABC):
     @_abc.abstractmethod
     async def dispatch(
-        self, data: str, context: _tp.Any, websocket: _tps.WriteWebsocket
+        self, data: str, context: _tp.Any, websocket: _rjwt.WriteWebsocket
     ) -> None:
         raise NotImplementedError()
 
 
 class SyncDispatcher(DispatcherBase):
     async def dispatch(
-        self, data: str, context: _tp.Any, websocket: _tps.WriteWebsocket
+        self, data: str, context: _tp.Any, websocket: _rjwt.WriteWebsocket
     ) -> None:
         if result := _jrpcs.dispatch(data, context=context):
             await websocket.send_str(result)
@@ -80,22 +121,22 @@ class AsyncTaskSpawningDispatcher(DispatcherBase):
         self._task_group = task_group
 
     async def dispatch(
-        self, data: str, context: _tp.Any, websocket: _tps.WriteWebsocket
+        self, data: str, context: _tp.Any, websocket: _rjwt.WriteWebsocket
     ) -> None:
         coroutine = self._dispatch_impl(data, context, websocket)
         self._task_group.create_task(coroutine)
 
     async def _dispatch_impl(
-        self, data: str, context: _tp.Any, websocket: _tps.WriteWebsocket
+        self, data: str, context: _tp.Any, websocket: _rjwt.WriteWebsocket
     ) -> None:
         if result := await _jrpcs.async_dispatch(data, context=context):
             await websocket.send_str(result)
 
 
-class JsonRpcServer(_tps.MessageReceiver):
+class JsonRpcServer(_rjwt.MessageReceiver):
     def __init__(
         self,
-        websocket: _tps.WriteWebsocket,
+        websocket: _rjwt.WriteWebsocket,
         dispatcher: DispatcherBase,
         context: _tp.Any,
     ) -> None:
